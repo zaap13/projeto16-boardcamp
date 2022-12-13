2921c1de7b54d3fefd0820872a6a0906142a119d
Felipe Bueno <felipebueno2201@gmail.com>
2022-12-13 03:46:38 -0300

    get rentals query

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -59,7 +59,64 @@ export async function postRent(req, res) {
 }
 
 export async function getRentals(req, res) {
+  const { customerId, gameId } = req.query;
+
+  console.log(customerId, gameId);
+
   try {
+    if (customerId) {
+      const rentals = await connection.query(
+        `SELECT rentals.*, 
+        JSON_BUILD_OBJECT('id', customers.id, 'name', customers.name) AS customer, 
+        JSON_BUILD_OBJECT('id', games.id, 'name', games.name, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game
+        FROM
+        rentals
+        JOIN
+        customers
+        ON
+        rentals."customerId"=customers.id
+        JOIN
+        games
+        ON
+        rentals."gameId" = games.id
+        JOIN
+        categories
+        ON
+        games."categoryId" = categories.id
+        WHERE customers.id = '${customerId}'
+        ORDER BY rentals.id
+
+     `
+      );
+      return res.send(rentals.rows);
+    } else if (gameId) {
+      const rentals = await connection.query(
+        `SELECT rentals.*, 
+        JSON_BUILD_OBJECT('id', customers.id, 'name', customers.name) AS customer, 
+        JSON_BUILD_OBJECT('id', games.id, 'name', games.name, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game
+        FROM
+        rentals
+        JOIN
+        customers
+        ON
+        rentals."customerId"=customers.id
+        JOIN
+        games
+        ON
+        rentals."gameId" = games.id
+        JOIN
+        categories
+        ON
+        games."categoryId" = categories.id
+        WHERE
+        games.id='${gameId}' 
+        ORDER BY rentals.id
+
+     `
+      );
+      return res.send(rentals.rows);
+    }
+
     const rentals = await connection.query(
       `SELECT rentals.*, 
       JSON_BUILD_OBJECT('id', customers.id, 'name', customers.name) AS customer, 
